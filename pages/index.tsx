ad808e10dc74876343c9ede3720b0057f1135a57
CODExSHUBHAM <shubham.nh10@gmail.com>
2023-12-30 22:55:51 +0530

    Added Movies List And Movie Cards

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,12 @@
+import useMovieList from '@/hooks/useMovieList';
+
+import { NextPageContext } from 'next';
+import { getSession } from 'next-auth/react'
 
 import Billboard from '@/components/Billboard';
+import MovieList from '@/components/MovieList';
 import Navbar from '@/components/Navbar';
-import useCurrentUser from '@/hooks/useCurrentUser';
-import { NextPageContext } from 'next';
-import { getSession, signOut } from 'next-auth/react'
+
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -24,12 +27,15 @@ export async function getServerSideProps(context: NextPageContext) {
 
 export default function Home() {
 
-  const { data: user } = useCurrentUser();
+  const { data: movies = [] } = useMovieList();
 
   return (
     <>
       <Navbar />
       <Billboard />
+      <div className='pb-40'>
+        <MovieList title='Trending' data={movies} />
+      </div>
     </>
   )
 }
